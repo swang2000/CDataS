c3bcede590ceecb896d7b4391c9dbc32b537e5a1
swang2000 <swang2000@gmail.com>
2021-08-25 16:23:55 -0700

    Commit on Aug 20

--- Backtracking/beautifularrangement.py
+++ Backtracking/beautifularrangement.py
@@ -18,3 +18,28 @@ Given an integer n, return the number of the beautiful arrangements that you can
 
 '''
 
+def beautifularrange(n):
+    cand = list(range(1, n+1))
+    temp =[]
+    rlist =[]
+
+    r =0
+    def dfs(temp, candx, i=1):
+        nonlocal r
+        if i == n+1:
+            rlist.append(temp[:])
+            r = r+1
+
+        for j, k in enumerate(candx):
+            if not (k%i and i%k):
+                temp.append(k)
+                dfs(temp, candx[:j]+candx[j+1:], i+1)
+                temp.pop()
+
+
+    dfs(temp, cand)
+    return r, rlist
+
+
+beautifularrange(4)
+
